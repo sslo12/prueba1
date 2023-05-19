b9d226d9a848caf2bd14d53e3ee5fa3dac40c2b9
ImBetterThanYesterday <99687072+ImBetterThanYesterday@users.noreply.github.com>
2023-05-18 21:55:34 -0500

    Update notificacion_Paquetes.php

--- microweb1/notificacion_Paquetes.php
+++ microweb1/notificacion_Paquetes.php
@@ -46,7 +46,7 @@
     </thead> 
     <tbody> 
     <?php 
-       $servurl="http://192.168.100.2:3003/alerta/$us";
+       $servurl="http://172.190.52.193:3003/alerta/$us";
        $curl=curl_init($servurl);
        
        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
