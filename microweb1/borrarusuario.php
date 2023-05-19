bbe0c6aa4009162e1f416341b095b86550b06e3e
ImBetterThanYesterday <99687072+ImBetterThanYesterday@users.noreply.github.com>
2023-05-18 22:39:50 -0500

    Update borrarusuario.php

--- microweb1/borrarusuario.php
+++ microweb1/borrarusuario.php
@@ -1,7 +1,7 @@
 <?php
 $id = $_POST["id"];
 
-$url = "http://192.168.100.2:3001/usuario/$id";
+$url = "http://172.190.52.193:3001/usuario/$id";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
